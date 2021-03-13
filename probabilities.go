608a8ba667f49c5e40a12d19211b67fefc2cd35f
Kelvin Zhang <me@iamkelv.in>
2021-03-13 14:17:09 +0000

    Sample independently without Dirichlet

--- probabilities.go
+++ probabilities.go
@@ -2,31 +2,31 @@ package main
 
 import (
 	"golang.org/x/exp/rand"
-	"gonum.org/v1/gonum/stat/distmv"
 	"time"
 )
 
 type ProbabilitySampler struct {
-	dirchlet *distmv.Dirichlet
+	n      int
+	random *rand.Rand
 }
 
 func NewProbabilitySampler(numVariables int) *ProbabilitySampler {
-	// Set up the Dirichlet distribution with a number of 1.0s corresponding to
-	// the number of variables according to https://stackoverflow.com/a/18662466/2102540.
-	var alpha []float64
-	for i := 0; i < numVariables; i++ {
-		alpha = append(alpha, 1.0)
-	}
-
 	// Set the random seed to the current time for sufficient uniqueness.
 	randSeed := uint64(time.Now().UTC().UnixNano())
 
 	return &ProbabilitySampler{
-		dirchlet: distmv.NewDirichlet(alpha, rand.NewSource(randSeed)),
+		n:      numVariables,
+		random: rand.New(rand.NewSource(randSeed)),
 	}
 }
 
-// Sample samples an array of probabilities adding up to 1.
+// Sample samples an array of probabilities.
 func (s *ProbabilitySampler) Sample() []float64 {
-	return s.dirchlet.Rand(nil)
+	var probs []float64
+
+	for i := 0; i < s.n; i++ {
+		probs = append(probs, s.random.Float64())
+	}
+
+	return probs
 }
